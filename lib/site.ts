071f30b14137fe38fbde6a8ac5c8d117d796420f
Michał Kotfica <kotvitz17@gmail.com>
2026-05-12 15:29:02 +0200

    chore: create initial project structure

---
+++ lib/site.ts
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  name: "Meble Ogrodowe Niewiadów",
+
+  company: {
+    legalName: "Sylwano Sylwester Popek",
+    address: "Szymanów 9A",
+    postalCode: "97-225",
+    city: "Ujazd",
+    nip: "7731333636",
+  },
+
+  contact: {
+    email: "sylwesterpopek@wp.pl",
+    phone: "+48602451798",
+    phoneDisplay: "+48 602 451 798",
+    tiktok: "https://www.tiktok.com/@sylwano.altany",
+    tiktokHandle: "@sylwano.altany",
+  },
+};
