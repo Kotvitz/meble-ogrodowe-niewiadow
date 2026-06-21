48271c4eb6c524c70570d5dc8bb56596eb406982
Michał Kotfica <kotvitz17@gmail.com>
2026-06-21 22:39:52 +0200

    feat: expand project gallery and improve image metadata

--- data/projects.ts
+++ data/projects.ts
@@ -154,4 +154,319 @@ export const projects = [
     image: "/images/project-31.webp",
     alt: "Nowoczesna altana z pionowymi lamelami",
   },
+  {
+    title: "Altana klasyczna",
+    image: "/images/project-32.webp",
+    alt: "Klasyczna altana ogrodowa wykonana z drewna konstrukcyjnego",
+  },
+  {
+    title: "Altana półzabudowana",
+    image: "/images/project-33.webp",
+    alt: "Drewniana altana półzabudowana z miejscem do wypoczynku",
+  },
+  {
+    title: "Altana kompaktowa",
+    image: "/images/project-34.webp",
+    alt: "Niewielka altana ogrodowa wykonana na zamówienie",
+  },
+  {
+    title: "Wiata rekreacyjna",
+    image: "/images/project-35.webp",
+    alt: "Drewniana wiata rekreacyjna z zabudowaną częścią boczną",
+  },
+  {
+    title: "Altana ogrodowa",
+    image: "/images/project-36.webp",
+    alt: "Drewniana altana ogrodowa z dachem czterospadowym",
+  },
+  {
+    title: "Altana z huśtawką",
+    image: "/images/project-37.webp",
+    alt: "Altana ogrodowa wyposażona w drewnianą huśtawkę",
+  },
+  {
+    title: "Altana z bali",
+    image: "/images/project-38.webp",
+    alt: "Altana wykonana z naturalnych bali drewnianych",
+  },
+  {
+    title: "Nowoczesna wiata",
+    image: "/images/project-39.webp",
+    alt: "Nowoczesna wiata drewniana w jasnym wykończeniu",
+  },
+  {
+    title: "Wiata samochodowa",
+    image: "/images/project-40.webp",
+    alt: "Drewniana wiata samochodowa z nowoczesną konstrukcją",
+  },
+  {
+    title: "Wiata garażowa",
+    image: "/images/project-41.webp",
+    alt: "Drewniana wiata garażowa wykonana na indywidualne zamówienie",
+  },
+  {
+    title: "Wiata dwustanowiskowa",
+    image: "/images/project-42.webp",
+    alt: "Duża drewniana wiata na dwa samochody",
+  },
+  {
+    title: "Altana przydomowa",
+    image: "/images/project-43.webp",
+    alt: "Altana ogrodowa z zabudowaną tylną ścianą",
+  },
+  {
+    title: "Altana rekreacyjna",
+    image: "/images/project-44.webp",
+    alt: "Przestronna altana ogrodowa do rodzinnego wypoczynku",
+  },
+  {
+    title: "Altana narożna",
+    image: "/images/project-45.webp",
+    alt: "Drewniana altana z częściową zabudową boczną",
+  },
+  {
+    title: "Altana klasyczna",
+    image: "/images/project-46.webp",
+    alt: "Klasyczna altana ogrodowa z drewnianą konstrukcją",
+  },
+  {
+    title: "Altana z podjazdem",
+    image: "/images/project-47.webp",
+    alt: "Altana ogrodowa przy kostce brukowej",
+  },
+  {
+    title: "Wiata gospodarcza",
+    image: "/images/project-48.webp",
+    alt: "Drewniana wiata gospodarcza z wydłużoną konstrukcją",
+  },
+  {
+    title: "Altana z bali",
+    image: "/images/project-49.webp",
+    alt: "Altana wykonana z naturalnych bali i drewna konstrukcyjnego",
+  },
+  {
+    title: "Altana ogrodowa",
+    image: "/images/project-50.webp",
+    alt: "Altana ogrodowa z wysokim dachem i zabudową",
+  },
+  {
+    title: "Altana drewniana",
+    image: "/images/project-51.webp",
+    alt: "Altana drewniana w ciemnym wybarwieniu",
+  },
+  {
+    title: "Altana z huśtawką",
+    image: "/images/project-52.webp",
+    alt: "Altana ogrodowa z huśtawką i stołem biesiadnym",
+  },
+  {
+    title: "Altana nowoczesna",
+    image: "/images/project-53.webp",
+    alt: "Nowoczesna altana z jasnym drewnem konstrukcyjnym",
+  },
+  {
+    title: "Altana z huśtawką",
+    image: "/images/project-54.webp",
+    alt: "Altana ogrodowa z miejscem do wypoczynku i huśtawką",
+  },
+  {
+    title: "Pergola ogrodowa",
+    image: "/images/project-55.webp",
+    alt: "Drewniana pergola ogrodowa",
+  },
+  {
+    title: "Altana tarasowa",
+    image: "/images/project-56.webp",
+    alt: "Altana połączona z przestrzenią tarasową",
+  },
+  {
+    title: "Wiata drewniana",
+    image: "/images/project-57.webp",
+    alt: "Wiata drewniana o solidnej konstrukcji",
+  },
+  {
+    title: "Altana zabudowana",
+    image: "/images/project-58.webp",
+    alt: "Altana z częściowo zabudowanymi ścianami",
+  },
+  {
+    title: "Altana półotwarta",
+    image: "/images/project-59.webp",
+    alt: "Półotwarta altana ogrodowa z drewnianym wykończeniem",
+  },
+  {
+    title: "Altana wypoczynkowa",
+    image: "/images/project-60.webp",
+    alt: "Altana wyposażona w meble ogrodowe",
+  },
+  {
+    title: "Altana rekreacyjna",
+    image: "/images/project-61.webp",
+    alt: "Drewniana altana rekreacyjna do ogrodu",
+  },
+  {
+    title: "Altana klasyczna",
+    image: "/images/project-62.webp",
+    alt: "Klasyczna altana ogrodowa w naturalnym kolorze drewna",
+  },
+  {
+    title: "Altana z zabudową",
+    image: "/images/project-63.webp",
+    alt: "Altana drewniana z częściową zabudową ścian",
+  },
+  {
+    title: "Altana przydomowa",
+    image: "/images/project-64.webp",
+    alt: "Altana wykonana przy domu jednorodzinnym",
+  },
+  {
+    title: "Zadaszenie tarasowe",
+    image: "/images/project-65.webp",
+    alt: "Drewniane zadaszenie tarasu",
+  },
+  {
+    title: "Altana z bali",
+    image: "/images/project-66.webp",
+    alt: "Altana wykonana z naturalnych bali drewnianych",
+  },
+  {
+    title: "Altana z meblami",
+    image: "/images/project-67.webp",
+    alt: "Altana wyposażona w drewniany stół i ławy",
+  },
+  {
+    title: "Meble ogrodowe",
+    image: "/images/project-68.webp",
+    alt: "Drewniany zestaw mebli ogrodowych",
+  },
+  {
+    title: "Altana leśna",
+    image: "/images/project-69.webp",
+    alt: "Altana położona w otoczeniu zieleni",
+  },
+  {
+    title: "Altana z huśtawką",
+    image: "/images/project-70.webp",
+    alt: "Altana ogrodowa z huśtawką drewnianą",
+  },
+  {
+    title: "Altana rustykalna",
+    image: "/images/project-71.webp",
+    alt: "Rustykalna altana wykonana z drewna",
+  },
+  {
+    title: "Altana ogrodowa",
+    image: "/images/project-72.webp",
+    alt: "Altana ogrodowa z zabudowaną częścią boczną",
+  },
+  {
+    title: "Altana z huśtawką",
+    image: "/images/project-73.webp",
+    alt: "Altana z drewnianą huśtawką i miejscem do biesiadowania",
+  },
+  {
+    title: "Zadaszenie ogrodowe",
+    image: "/images/project-74.webp",
+    alt: "Drewniane zadaszenie przestrzeni wypoczynkowej",
+  },
+  {
+    title: "Wiata rekreacyjna",
+    image: "/images/project-75.webp",
+    alt: "Drewniana wiata rekreacyjna z otwartą konstrukcją",
+  },
+  {
+    title: "Altana premium",
+    image: "/images/project-76.webp",
+    alt: "Duża altana ogrodowa w eleganckim wykończeniu",
+  },
+  {
+    title: "Nowoczesna wiata",
+    image: "/images/project-77.webp",
+    alt: "Nowoczesna biała wiata drewniana",
+  },
+  {
+    title: "Altana przydomowa",
+    image: "/images/project-78.webp",
+    alt: "Altana wykonana przy posesji prywatnej",
+  },
+  {
+    title: "Altana z huśtawką",
+    image: "/images/project-79.webp",
+    alt: "Altana ogrodowa z huśtawką i stołem",
+  },
+  {
+    title: "Pergola drewniana",
+    image: "/images/project-80.webp",
+    alt: "Drewniana pergola do ogrodu",
+  },
+  {
+    title: "Huśtawka ogrodowa",
+    image: "/images/project-81.webp",
+    alt: "Drewniana huśtawka ogrodowa wykonana na zamówienie",
+  },
+  {
+    title: "Altana zielona",
+    image: "/images/project-82.webp",
+    alt: "Altana z zielonym wykończeniem elewacji",
+  },
+  {
+    title: "Altana kompaktowa",
+    image: "/images/project-83.webp",
+    alt: "Kompaktowa altana ogrodowa z zabudową",
+  },
+  {
+    title: "Altana z bali",
+    image: "/images/project-84.webp",
+    alt: "Altana z naturalnych bali drewnianych",
+  },
+  {
+    title: "Pergola ogrodowa",
+    image: "/images/project-85.webp",
+    alt: "Pergola drewniana do aranżacji ogrodu",
+  },
+  {
+    title: "Wiata gospodarcza",
+    image: "/images/project-86.webp",
+    alt: "Wiata drewniana z zabudowaną częścią magazynową",
+  },
+  {
+    title: "Altana nowoczesna",
+    image: "/images/project-87.webp",
+    alt: "Nowoczesna altana z czarnymi lamelami",
+  },
+  {
+    title: "Altana premium",
+    image: "/images/project-88.webp",
+    alt: "Duża altana ogrodowa w nowoczesnym stylu",
+  },
+  {
+    title: "Altana z huśtawką",
+    image: "/images/project-89.webp",
+    alt: "Altana wyposażona w drewnianą huśtawkę",
+  },
+  {
+    title: "Wiata rekreacyjna",
+    image: "/images/project-90.webp",
+    alt: "Drewniana wiata rekreacyjna z miejscem wypoczynkowym",
+  },
+  {
+    title: "Zadaszenie tarasowe",
+    image: "/images/project-91.webp",
+    alt: "Drewniane zadaszenie tarasu przy domu",
+  },
+  {
+    title: "Pergola ogrodowa",
+    image: "/images/project-92.webp",
+    alt: "Pergola ogrodowa w jasnym wykończeniu",
+  },
+  {
+    title: "Altana kompaktowa",
+    image: "/images/project-93.webp",
+    alt: "Niewielka altana ogrodowa z zabudową",
+  },
+  {
+    title: "Altana drewniana",
+    image: "/images/project-94.webp",
+    alt: "Drewniana altana wykonana na indywidualne zamówienie",
+  },
 ];
